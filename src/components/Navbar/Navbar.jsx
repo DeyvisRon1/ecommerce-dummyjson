d497774e157f7f8ba062797fdbc86d5acae9ca04
Deyvis <deyvis.salas@correoaiep.cl>
2026-07-01 01:39:54 -0400

    README

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css';
 function Navbar({ cartCount }) {
   return (
     <nav className="navbar">
-      <span className="navbar__logo">Mi E-commerce</span>
+      <span className="navbar__logo">Mi E-commerce Deyvis Salas</span>
       <span className="navbar__cart">🛒 {cartCount}</span>
     </nav>
   );
